c1e4cf9f1a11eeda5d167b495041441bf00032ad
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-06-04 18:31:20 +0200

    created a staging table named dim_customer

--- 2.0.stg_dim_location.sql
+++ 2.0.stg_dim_location.sql
@@ -11,7 +11,8 @@ BEGIN
         Location_ID INT IDENTITY(1,1) PRIMARY KEY,
         Continent VARCHAR(100) NULL,
         Country_or_State VARCHAR(100) NULL,
-        Province_or_City VARCHAR(100) NULL
+        Province_or_City VARCHAR(100) NULL,
+         created_at DATETIME DEFAULT GETDATE()
     );
 END;
 GO
@@ -26,3 +27,5 @@ SELECT DISTINCT
     Country_or_State,
     Province_or_City
 FROM stg_pc_sales.dbo.Raw_Pc_Data;
+
+drop table stg_pc_sales.dbo.dim_location;
